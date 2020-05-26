ba966fd5440f5238b0265b87c52579e23182a2f7
Richie Goulazian <rgoulazian@gmail.com>
2020-05-26 17:49:34 -0400

    Fix according to Google Style Guide

--- portfolio/src/main/webapp/js/activities.js
+++ portfolio/src/main/webapp/js/activities.js
@@ -1,7 +1,7 @@
+let scroller = null;
 /**
  * Gathers repos from GitHub and scrolls through them in the About Me section
  */
-let scroller = null;
 async function startActivitiesScroll() {
   let repos = await fetch("https://api.github.com/users/Richie78321/repos")
     .then(res => res.json());
@@ -12,38 +12,73 @@ async function startActivitiesScroll() {
   scroller.start();
 }
 
-function Scroller(repos) {
-  this.repos = repos;
-  this.index = 0;
-  this.timeoutID = null;
+/**
+ * Scrolls through a set of GitHub repository descriptions at a set speed.
+ */
+class Scroller {
+  /**
+   * @param {Array<Object>} repos 
+   */
+  constructor(repos) {
+    /**
+     * Collection of user GitHub repos
+     * @type {Array<Object>}
+     */
+    this.repos = repos;
+
+    /** @private */
+    this._index = 0;
+
+    /** @private */
+    this._timeoutID = null;;
+  }
+
+  /**
+   * Increments the scrolling activity 
+   * @private
+   */
+  _scrollActivity() {
+    const NEXT_TIMEOUT = 5000;
+    const repo = this.repos[this._index];
+
+    const activityContainer = document.getElementById('activity-scroller');
+    activityContainer.innerText = repo.description;
+
+    // Increment index and wait to update scroller
+    this._index++;
+    this._index %= this.repos.length;
+    this._timeoutID =
+      setTimeout(() => { this._scrollActivity(); }, NEXT_TIMEOUT);
+  }
+
+  /**
+   * Starts the scrolling effect
+   */
+  start() {
+    if (this.isPaused()) {
+      this._scrollActivity();
+    }
+  }
+
+
+  /**
+   * Pauses the scrolling effect 
+   */
+  pause() {
+    if (!this.isPaused()) {
+      clearTimeout(this._timeoutID);
+      this._timeoutID = null;
+    }
+  }
+
+  /**
+   * Determines if the scroller is currently paused
+   * @returns {boolean}
+   */
+  isPaused() {
+    return !this._timeoutID;
+  }
 }
-Scroller.prototype.activityScroll = function() {
-  const nextTimeout = 5000;
-  const repo = this.repos[this.index];
-
-  const activityContainer = document.getElementById('activity-scroller');
-  activityContainer.innerText = repo.description;
-
-  // Increment index and wait to update scroller
-  this.index++;
-  this.index %= this.repos.length;
-  this.timeoutID =
-    setTimeout(() => { this.activityScroll(); }, nextTimeout);
-};
-Scroller.prototype.start = function() {
-  if (!this.timeoutID) {
-    this.activityScroll();
-  }
-};
-Scroller.prototype.pause = function() {
-  if (this.timeoutID) {
-    clearTimeout(this.timeoutID);
-    this.timeoutID = null;
-  }
-};
-Scroller.prototype.isPaused = function() {
-  return !this.timeoutID;
-};
 
 /**
  * Toggles activity scrolling feature
