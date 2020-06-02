2c160c68afe489e0aced942f9184594fef6ca226
Richie Goulazian <rgoulazian@gmail.com>
2020-06-02 23:04:36 +0000

    Improve comment UI integration

--- portfolio/src/main/webapp/js/comments.js
+++ portfolio/src/main/webapp/js/comments.js
@@ -2,19 +2,33 @@
  * Gets comments from the '/comments' endpoint
  * and displays them in the comments section.
  */
-function getComments() {
+function loadComments() {
   fetch('/comments')
     .then(resp => resp.json())
     .then(comments => {
       console.log("Received comments: ");
       console.log(comments);
 
+      removeCommentsOnPage();
       comments.forEach((comment) => addCommentToPage(comment));  
     })
     .catch(err => {
       console.error(err);
       alert("Failed to populate comments.");
     });
+}
+
+/**
+ * Removes the comments currently displaying on the page.
+ */
+function removeCommentsOnPage() {
+  const commentContainer = document.getElementById("comment-container");
+
+  let lastComment = commentContainer.lastElementChild;
+  while (lastComment) {
+    commentContainer.removeChild(lastComment);
+    lastComment = commentContainer.lastElementChild;
+  }
 }
 
 /**
@@ -26,10 +40,15 @@ function addCommentToPage(comment) {
   
   // Create a new comment element
   const newComment = document.createElement("div");
-  newComment.classList.add("card");
-  newComment.classList.add("p-2");
-  newComment.classList.add("my-2");
+  newComment.classList.add("p-4");
+  newComment.classList.add("border-bottom");
   newComment.innerText = comment.commentBody;
+
+  const authorFooter = document.createElement("footer");
+  authorFooter.classList.add("blockquote-footer");
+  authorFooter.innerText = comment.author;
+
+  newComment.appendChild(authorFooter);
 
   // Add it to the page
   commentContainer.appendChild(newComment);
