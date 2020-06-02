4a214adc14e6400f2e05308b28aa5e43be448eb6
Richie Goulazian <rgoulazian@gmail.com>
2020-06-02 14:34:37 +0000

    Setup delivery of sample comment data to index.html

--- portfolio/src/main/webapp/js/script.js
+++ portfolio/src/main/webapp/js/script.js
@@ -32,18 +32,38 @@ function addRandomFact() {
 }
 
 /**
- * Requests the data from the '/data' endpoint
- * and adds it to the data container.
+ * Gets comments from the '/comments' endpoint
+ * and displays them in the comments section.
  */
-function getData() {
-  const dataContainer = document.getElementById("data-container");
-  fetch('/data')
-    .then(resp => resp.text())
-    .then(text => {
-      dataContainer.innerText = text;
+function getComments() {
+  fetch('/comments')
+    .then(resp => resp.json())
+    .then(comments => {
+      console.log("Received comments: ");
+      console.log(comments);
+
+      comments.forEach((comment) => addCommentToPage(comment));  
     })
     .catch(err => {
       console.error(err);
-      dataContainer.innerText = "Error fetching data.";
+      alert("Failed to populate comments.");
     });
+}
+
+/**
+ * Adds a comment to the comments section UI.
+ * @param {{author: string, commentBody: string}} comment
+ */
+function addCommentToPage(comment) {
+  const commentContainer = document.getElementById("comment-container");
+  
+  // Create a new comment element
+  const newComment = document.createElement("div");
+  newComment.classList.add("card");
+  newComment.classList.add("p-2");
+  newComment.classList.add("my-2");
+  newComment.innerText = comment.commentBody;
+
+  // Add it to the page
+  commentContainer.appendChild(newComment);
 }
