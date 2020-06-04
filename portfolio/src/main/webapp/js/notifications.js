9d7b2b11a8ab2a0ce409fb54ed2341570d19f3d3
Richie Goulazian <rgoulazian@gmail.com>
2020-06-04 15:27:18 +0000

    Add JS notification system

---
+++ portfolio/src/main/webapp/js/notifications.js
@@ -0,0 +1,29 @@
+/**
+ * Adds a notification to the screen with a specified display time.
+ * @param {string} msg
+ * @param {string} alertClass The class of the alert. Useful for applying style.
+ * @param {number} displayTime Time to display the alert in milliseconds.
+ */
+function addNotification(msg, alertClass, displayTime) {
+  const NOTIFICATION_FADE_OUT_TIME = 1000; // 1 second
+  const notificationContainer = document.getElementById("notification-container");
+
+  const notificationElement = document.createElement("div");
+  notificationElement.classList.add("notification");
+  notificationElement.classList.add(alertClass);
+  notificationElement.classList.add("p-2");
+  notificationElement.classList.add("mb-2");
+  notificationElement.classList.add("w-100");
+
+  notificationElement.innerText = msg;
+
+  notificationContainer.appendChild(notificationElement);
+
+  setTimeout(() => {
+    notificationElement.style.opacity = "0";
+  }, displayTime);
+
+  setTimeout(() => {
+    notificationContainer.removeChild(notificationElement);
+  }, displayTime + NOTIFICATION_FADE_OUT_TIME);
+}
