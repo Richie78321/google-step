0f1a9ea83653f498255605cd24ee4a70c64ce315
Richie Goulazian <rgoulazian@gmail.com>
2020-06-11 17:38:54 +0000

    Add find meeting query functionality

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,140 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
 
 public final class FindMeetingQuery {
+  /**
+   * Get the collection of time ranges when an event can be held.
+   * @param events
+   * @param request
+   * @return Returns the collection of time ranges when the event can be held.
+   */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+      return Arrays.asList();
+    }
+
+    events = getEventsAttending(events, request.getAttendees());
+    Collection<TimeRange> availableTimes = new ArrayList<TimeRange>();
+    availableTimes.add(TimeRange.WHOLE_DAY);
+
+    // Each event time range punches a hole in available time range
+    Iterator<Event> eventIterator = events.iterator();
+    Event event;
+    while (eventIterator.hasNext()) {
+      event = eventIterator.next();
+      TimeRange eventTimeRange = event.getWhen();
+
+      Collection<TimeRange> newRanges = new ArrayList<TimeRange>();
+      Collection<TimeRange> rangesToRemove = new ArrayList<TimeRange>();
+      Iterator<TimeRange> availableTimesIterator = availableTimes.iterator();
+      TimeRange availableTime;
+      while (availableTimesIterator.hasNext()) {
+        availableTime = availableTimesIterator.next();
+
+        if (availableTime.overlaps(eventTimeRange)) {
+          removeRange(
+              newRanges, rangesToRemove, availableTime, eventTimeRange, request.getDuration());
+        }
+      }
+
+      availableTimes.removeAll(rangesToRemove);
+      availableTimes.addAll(newRanges);
+    }
+
+    return availableTimes;
+  }
+
+  /**
+   * Removes a range of time from a time range.
+   * Precondition: The ranges are assumed to be overlapping.
+   *
+   * Broken down into three different scenarios:
+   * 1. The time range is fully contained in the range to remove: just remove the time range.
+   * 2. The range to remove is fully contained in the time range: split the time range into two
+   * pieces.
+   * 3. The time range overlaps with the range to remove but neither is fully contained in the
+   * other: trim the time range.
+   * 
+   * @param newRanges
+   * @param discardedRanges
+   * @param timeRange
+   * @param rangeToRemove
+   * @param minDuration
+   */
+  private void removeRange(
+      Collection<TimeRange> newRanges,
+      Collection<TimeRange> discardedRanges,
+      TimeRange timeRange, 
+      TimeRange rangeToRemove, 
+      long minDuration) {
+    discardedRanges.add(timeRange);
+    if (rangeToRemove.contains(timeRange)) {
+      return;
+    }
+    
+    if (timeRange.contains(rangeToRemove)) {
+      // Split time
+      addTimeRangeIfLongEnough(newRanges, timeRange.start(), rangeToRemove.start(), minDuration);
+      addTimeRangeIfLongEnough(newRanges, rangeToRemove.end(), timeRange.end(), minDuration);
+    }
+    else {
+      if (timeRange.start() < rangeToRemove.start()) {
+        addTimeRangeIfLongEnough(
+            newRanges, timeRange.start(), rangeToRemove.start(), minDuration);
+      } else {
+        addTimeRangeIfLongEnough(newRanges, rangeToRemove.end(), timeRange.end(), minDuration);
+      }
+    }
+  }
+
+  /**
+   * Adds a time range to the collection if it meets the specified minimum duration
+   * @param newRanges
+   * @param startTime
+   * @param endTime
+   * @param minDuration
+   */
+  private void addTimeRangeIfLongEnough(
+      Collection<TimeRange> newRanges, int startTime, int endTime, long minDuration) {
+    if (endTime - startTime >= minDuration) {
+      newRanges.add(TimeRange.fromStartEnd(startTime, endTime, false));
+    }
+  }
+
+  /**
+   * Gets all of the events being attended by at least one of the attendees.
+   * @param events
+   * @param attendees
+   * @return Returns a collection of events being attended by at least one of the attendees.
+   */
+  private Collection<Event> getEventsAttending(
+      Collection<Event> events, Collection<String> attendees) {
+    Collection<Event> attendingEvents = new ArrayList<Event>();
+
+    Iterator<Event> eventsIterator = events.iterator();
+    Event event;
+    while (eventsIterator.hasNext()) {
+      event = eventsIterator.next();
+      Set<String> eventAttendees = event.getAttendees();
+
+      Iterator<String> attendeesIterator = attendees.iterator();
+      String attendee;
+      while (attendeesIterator.hasNext()) {
+        attendee = attendeesIterator.next();
+        
+        if (eventAttendees.contains(attendee)) {
+          attendingEvents.add(event);
+          break;
+        }
+      }
+    }
+
+    return attendingEvents;
   }
 }
